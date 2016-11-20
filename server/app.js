84231dae3d865bafccc7b819e3b4c80883b6c434
Scott Bromander <scott@primeacademy.io>
2016-11-19 21:00:03 -0600

    initial commit

---
+++ server/app.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var socketIO = require('socket.io');
+var fs = require('fs');
+var server;
+var io;
+
+server = http.createServer(function(req,res){
+    fs.readFile(__dirname + '/index.html', function(err,data){
+        res.writeHead(200);
+        res.end(data);
+    });
+});
+
+server.listen(5000);
+io = socketIO(server);
+
+io.on('connection', function(socket){
+  
+
+  socket.emit('greeting-from-server', {
+    greeting: 'Hello Client'
+  });
+
+  socket.on('greeting-from-client', function(message){
+    console.log(message);
+  });
+});
